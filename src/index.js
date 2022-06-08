2ac2424897b183d8e5285a386e23130db396cb85
lissalad <elissalaymon@gmail.com>
2022-06-08 15:00:41 -0700

    first

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+function convert(color, code) {
+  switch(code) {
+    case "hex":
+      console.log("hex!");
+      break;
+    case "rgb":
+      console.log("rgb!");
+      break;
+    case "hsl":
+      console.log("hsl!");
+      break;
+    case "cmyk":
+      console.log("cymk!");
+      break;
+    case "name":
+      console.log("name!");
+      break;
+  }
+}
+
+// returns true if color is black
+function isBlack(str) {
+
+}
+
+convert("","hex");
