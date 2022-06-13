56c8450fb575ef3ee897e30dd222e36d2eee5ecc
lissalad <elissalaymon@gmail.com>
2022-06-13 15:49:04 -0700

    fixed and completed both rgb to hex and back

--- src/index.js
+++ src/index.js
@@ -3,47 +3,56 @@ const colorNames = require("./webColors.json");
 function convert(color, input, output) {
   let hex;
 
+  // convert all to hex
   switch (input) {
-    // return same if converting to same code
+    // unless converting to same code
     case output:
       return color;
 
-    // convert all to hex
     case "hex":
       console.log("input: hex!");
       hex = color;
       break;
+
     case "rgb":
       console.log("input: rgb!");
       break;
+
     case "hsl":
       console.log("input: hsl!");
       break;
+
     case "cmyk":
       console.log("input: cymk!");
       break;
+
     case "name":
       console.log("input: name!");
       hex = convertNameToHex(color);
       break;
   }
 
-  // otherwise convert hex to requested output
+  // convert hex to requested output and return value
   switch (output) {
-    // convert all from hex to desired output
+    // unless hex IS requested output
+    case "hex":
+      return hex;
+
     case "rgb":
       console.log("output: rgb!");
       break;
+
     case "hsl":
       console.log("output: hsl!");
       break;
+
     case "cmyk":
       console.log("output: cymk!");
       break;
+      
     case "name":
       console.log("output: name!");
       return convertHexToName(hex);
-    case "hex":
   }
 }
 
@@ -56,14 +65,20 @@ function convertNameToHex(color) {
 }
 
 // in progress
-function convertRGBToHex(color) {}
+function convertRGBToHex(color) { 
+
+  const hex = color.map((v) => {
+    return(v.toString(16));
+  });
+
+  return `#${hex.join("")}`;
+}
 
 function convertHSLToHex(color) {}
 
 function convertCYMKToHex(color) {}
 
 // ------- convert hex to desired code output --------------------- //
-
 // complete
 function convertHexToName(color) {
   const index = Object.values(colorNames).indexOf(color);
@@ -72,9 +87,10 @@ function convertHexToName(color) {
 
 // complete
 function convertHexToRGB(color) {
-  let rgb = [];
-  color.forEach((v) => {
-    rgb.push(parseInt(v, 16));
+  let values = color.slice(1).match(/.{1,2}/g);
+
+  const rgb = values.map((v) => {
+    return parseInt(v, 16);
   });
 
   return rgb;
@@ -98,11 +114,5 @@ function initializeHex(color, input, output) {
 }
 
 // LISSA'S TESTS
-// console.log(convert("#ff0000", "hex", "name"));
-// console.log(parseInt("C2", 16));
-// const str = "abcd";
-// const r = str.slice(0,2);
-// console.log(str);
-// console.log(convertHexToName("#8a2be2"));
-// console.log(convertNameToHex("red"));
-console.log(convertHexToRGB([44, 60, "c5"]));
+
+console.log(convertRGBToHex([254, 227, 62]));
